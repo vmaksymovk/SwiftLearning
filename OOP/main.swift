b70ef76009d16f95d96d593b991ea35bba0266e8
Vladyslav Maksymov <vmaksymovk@gmail.com>
2023-05-02 16:18:53 +0200

    Initial Commit

---
+++ OOP/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  OOP
+//
+//  Created by Влад on 5/2/23.
+//
+
+import Foundation
+
+class className{
+    var num1 : Int = 0
+    
+    func someFunc(num1 : Int = 10, num2 : Int = 20) -> Int {
+        return self.num1 + num2
+    }
+}
