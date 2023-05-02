ff662a43059225b0ea6e61dd445abec5ccd01b01
Vladyslav Maksymov <vmaksymovk@gmail.com>
2023-05-02 16:26:24 +0200

    I added init in Class

--- OOP/main.swift
+++ OOP/main.swift
@@ -8,13 +8,16 @@
 import Foundation
 
 class className{
-    var num1 : Int = 0
+    var num1 : Int
     
     func someFunc(num1 : Int = 10, num2 : Int = 20) -> Int {
         return self.num1 + num2
+    }
+    init(num1: Int) {
+        self.num1 = num1
     }
 }
 
 var obj = className()
 print(obj.someFunc())
-///
+
