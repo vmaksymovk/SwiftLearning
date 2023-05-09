9308404e7584b5f3bdaa391f0c442c0e249f6106
Vladyslav Maksymov <vmaksymovk@gmail.com>
2023-05-09 15:19:33 +0200

    inheritance

--- OOP/main.swift
+++ OOP/main.swift
@@ -1,37 +1,41 @@
-//
-//  main.swift
-//  OOP
-//
-//  Created by Влад on 5/2/23.
-//
-
 import Foundation
 
 
-// deinit
+class RegularPolygon {
 
-
-class test{
-    var name : String
-    
-    init(name: String) {
-        print("We called initialization")
-        self.name = name
-    }
-    
-    convenience init(){
-        self.init(name: "Vladyslav")
-        print("We called convinience init and usually init \(self.name)")
-    }
-    
-    deinit {
-        print("When instance == nil, we called deinitialization ")
-    }
-    
-    
+ func calculatePerimeter(length: Int, sides: Int) {
+   var result = length * sides
+   print("Perimeter:", result )
+ }
 }
 
+// inherit Square from Polygon
+class RegularSquare: RegularPolygon {
+ var length = 0
+ var sides = 0
 
-var obj : test? = test()
+ func calculateArea() {
+   var area = length * length
+   print("Regular Square Area:", area)
+ }
+}
 
-obj = nil
+// inherit Pentagon from Polygon
+class RegularTriangle: RegularPolygon {
+ var length = 0.0
+ var sides = 0.0
+
+ func calculateArea() {
+   var area = (sqrt(3)/4) * (length * length)
+   print("Regular Triangle Area:", area)
+ }
+}
+var shape = RegularSquare()
+shape.length = 4
+shape.calculateArea()
+shape.calculatePerimeter(length: 3,sides:4)
+
+var shape2 = RegularTriangle()
+shape2.length = 2
+shape2.calculateArea()
+shape2.calculatePerimeter(length: 2,sides:3)
